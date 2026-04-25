c1976ecdd49632178c51eb06ee9005e9fee0d70f
Saeghxx <alina.retiznik2007@gmail.com>
2026-04-25 22:57:08 +0300

    add promise-based async filter implementation

--- Labop5/filter.js
+++ Labop5/filter.js
@@ -63,3 +63,66 @@ function filterCallback(arr, predicate, cb, signal) {
     timers.push(t);
   }
 }
+function filterPromise(arr, predicate, signal) {
+  return new Promise((resolve, reject) => {
+    const timers = [];
+    const results = new Map();
+    let finished = false;
+    let aborted = signal.aborted;
+
+    function cleanup() {
+      timers.forEach(clearTimeout);
+      signal.removeEventListener("abort", onAbort);
+    }
+
+    function onAbort() {
+      aborted = true;
+      finish(new AbortError());
+    }
+
+    function finish(err, value) {
+      if (finished) return;
+      finished = true;
+      cleanup();
+
+      if (err) reject(err);
+      else resolve(value);
+    }
+
+    signal.addEventListener("abort", onAbort);
+
+    if (aborted) return finish(new AbortError());
+
+    let done = 0;
+
+    (async () => {
+      for (const [i, item] of arr.entries()) {
+        const t = setTimeout(async () => {
+          if (aborted || finished) return;
+
+          try {
+            const ok = await predicate(item, i);
+
+            if (aborted || finished) return;
+
+            if (ok) results.set(i, item);
+
+            done++;
+
+            if (done === arr.length) {
+              const ordered = [...results.entries()]
+                .sort((a, b) => a[0] - b[0])
+                .map(([_, v]) => v);
+
+              finish(null, ordered);
+            }
+          } catch (e) {
+            finish(e);
+          }
+        }, 0);
+
+        timers.push(t);
+      }
+    })();
+  });
+}
