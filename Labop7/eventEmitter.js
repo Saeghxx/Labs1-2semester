5842bf37be3dcf8205ab661f29b3752c8fc293ee
Saeghxx <alina.retiznik2007@gmail.com>
2026-04-28 22:09:23 +0300

    subscribe/unsubscribe

--- Labop7/eventEmitter.js
+++ Labop7/eventEmitter.js
@@ -2,4 +2,19 @@ class EventEmitter {
   constructor() {
     this.subscribers = {};
   }
+
+  subscribe(event, callback) {
+  if (!this.subscribers[event]) {
+    this.subscribers[event] = [];
+  }
+  this.subscribers[event].push(callback);
+}
+
+unsubscribe(event, callback) {
+  if (!this.subscribers[event]) return;
+
+  this.subscribers[event] = this.subscribers[event].filter(
+    sub => sub !== callback
+  );
+ }
 }
