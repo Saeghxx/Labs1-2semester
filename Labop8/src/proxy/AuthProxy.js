af52bd1478e33f51c322a99af4f2cfa9592535e8
Saeghxx <alina.retiznik2007@gmail.com>
2026-05-02 10:10:51 +0300

    almost finished lab

--- Labop8/src/proxy/AuthProxy.js
+++ Labop8/src/proxy/AuthProxy.js
@@ -7,21 +7,25 @@ export class AuthProxy {
   async request(req) {
     const headers = await this.strategy.apply(req.headers || {});
 
-    try {
-      return await this.client.request({
-        ...req,
-        headers,
+    let response = await this.client.request({
+      ...req,
+      headers,
+    });
+
+    if (response.status === 401 && this.strategy.refresh) {
+      const newToken = await this.strategy.refresh();
+
+      const refreshedHeaders = await this.strategy.apply({
+        ...(req.headers || {}),
+        Authorization: `Bearer ${newToken}`,
       });
-    } catch (err) {
-    
-      if (err.status === 401 && this.strategy.refresh) {
-        await this.strategy.refresh();
 
-        const newHeaders = await this.strategy.apply(req.headers || {});
-        return this.client.request({ ...req, headers: newHeaders });
-      }
-
-      throw err;
+      response = await this.client.request({
+        ...req,
+        headers: refreshedHeaders,
+      });
     }
+
+    return response;
   }
 }
